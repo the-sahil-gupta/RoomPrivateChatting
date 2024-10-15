b5f87ea9bdceb4f867ff66bcb615a90150f741da
the-sahil-gupta <sahilgtech95@gmail.com>
2024-10-15 23:52:40 +0530

    demo

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const http = require('http');
+const socketIO = require('socket.io');
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+	console.log('New User: ', socket.id);
+});
 
 app.get('/', (req, res) => {
 	res.render('index');
