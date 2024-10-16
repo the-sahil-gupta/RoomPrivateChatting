f519270bcf058ba27a7a9b5869674357d76ec567
the-sahil-gupta <sahilgtech95@gmail.com>
2024-10-17 00:26:48 +0530

    successfull connection users in a single room

--- app.js
+++ app.js
@@ -1,24 +1,38 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 app.set('view engine', 'ejs');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
 const http = require('http');
-const socketIO = require('socket.io');
 const server = http.createServer(app);
+const socketIO = require('socket.io');
 const io = socketIO(server);
 
+const userid = [];
+
 io.on('connection', (socket) => {
-	console.log('New User: ', socket.id);
+	if (userid.length > 0) {
+		const roomID = uuidv4();
+		socket.join(roomID);
+		socket.join(userid[0]);
+		console.log(`Users joined room: ${roomID}`);
+		userid.pop();
+	} else {
+		userid.push(socket.id);
+		console.log(`User added with socket ID: ${socket.id}`);
+	}
 });
 
 app.get('/', (req, res) => {
 	res.render('index');
 });
 
-app.listen(3000);
+server.listen(3000, () => {
+	console.log('Server is running on port http://localhost:3000');
+});
